ca6fea007ece211b299a10dcb4da831ff58355cd
Eric Barns <eric@abstractapi.com>
2020-11-03 15:51:49 +0100

    Changed project version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='abstract-ip-geolocation-api',
-    version='1.5.0',
+    version='1.5.1',
     description='IP Geolocation API from Abstract to geolocate any IP',
     long_description_content_type="text/markdown",
     long_description=README,
@@ -14,8 +14,11 @@ setup_args = dict(
     author='Eric Barns',
     author_email='eric@abstractapi.com',
     keywords=['IP geolocation', 'IP address', 'IP geolocation API', 'geolocation', 'ip location', 'geocoding'],
-    url='https://github.com/geolocation-api/ip-geolocation-api-python/',
-    download_url='https://pypi.org/project/abstract-ip-geolocation-api/'
+    download_url='https://pypi.org/project/abstract-ip-geolocation-api/',
+    project_urls={
+        'Documentation': 'https://www.abstractapi.com/ip-geolocation-api',
+        'Source': 'https://github.com/geolocation-api/ip-geolocation-api-python/'
+    }
 )
 
 install_requires = []
